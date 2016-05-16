43f59a152bfab2578325adb5cc9721d2f32c9d14
Miguel Eduardo Gil Biraud <miguel.gil.biraud@ieee.org>
2016-05-16 14:32:16 +0200

    Add TimeBin

--- time.go
+++ time.go
@@ -7,3 +7,14 @@ import (
 var (
 	Now = func() time.Time { return time.Now() }
 )
+
+type TimeBin int
+
+func TimeToHourBin(t time.Time) TimeBin {
+	return TimeToBin(t, time.Hour)
+}
+
+func TimeToBin(t time.Time, d time.Duration) TimeBin {
+	return TimeBin(t.Truncate(d).Unix())
+
+}
